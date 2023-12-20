9d3cdd3addb09fda487599afe0d74b2103c54054
Kshitij Bhardwaj <tothemoon202154@outlook.com>
2023-12-20 12:56:08 +0530

    Completed dashboard with all services link

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -6,7 +6,22 @@ import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 import Sidebar from "@/components/sidebar";
 
+import { useEffect, useState } from "react";
+
 const MobileSidebar = () => {
+
+
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, [])
+
+    if(!isMounted){
+        return null;
+    }
+
+
     return(
         <Sheet>
 
